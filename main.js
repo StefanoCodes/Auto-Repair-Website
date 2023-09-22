792657b7351e762a1eaa985bb772ee4244056d43
stefanovidmar2005 <stefanovidmar@gmail.com>
2023-09-22 16:41:02 +0300

    updated app logo

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const hamburgerToggle = document.querySelector(".hamburger-menu");
 const menu = document.querySelector(".menu");
 const body = document.body;
-const bottomBar = document.querySelector(".bar-bottom");
-const middleBar = document.querySelector(".bar-middle");
 hamburgerToggle.addEventListener("click", function () {
   menu.classList.toggle("active");
   hamburgerToggle.classList.toggle("close");
