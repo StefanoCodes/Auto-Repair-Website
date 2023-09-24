e657e8e51c808bc39a731437fb7187383cdf556a
stefanovidmar2005 <stefanovidmar@gmail.com>
2023-09-24 19:01:42 +0300

    refactoring of js code

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const hamburgerToggle = document.querySelector(".hamburger-menu");
 const menu = document.querySelector(".menu");
 const body = document.body;
-hamburgerToggle.addEventListener("click", function () {
-  // for accessibility
+const navLinks = document.querySelectorAll(".nav_link");
+
+// OPEN/CLOSE Drawer
+const toggleDrawer = () => {
   const expanded =
     hamburgerToggle.getAttribute("aria-expanded") === "true" || false;
   hamburgerToggle.setAttribute("aria-expanded", !expanded);
   menu.classList.toggle("active");
   hamburgerToggle.classList.toggle("close");
   body.classList.toggle("overflow-hidden");
+};
+hamburgerToggle.addEventListener("click", toggleDrawer);
+// to avoid a user clicking in the drawer menu and then getting stuck since the overflow would be hidden so we are automatically closing the drawer upon a click
+// TODO: Implement Event delegation instead
+navLinks.forEach((link) => {
+  link.addEventListener("click", toggleDrawer);
 });
