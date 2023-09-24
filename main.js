d3af0c8303aae603a0d34e94853b50e271300093
stefanovidmar2005 <stefanovidmar@gmail.com>
2023-09-24 18:48:30 +0300

    Services Section Mobile Responsive

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const hamburgerToggle = document.querySelector(".hamburger-menu");
 const menu = document.querySelector(".menu");
 const body = document.body;
 hamburgerToggle.addEventListener("click", function () {
+  // for accessibility
+  const expanded =
+    hamburgerToggle.getAttribute("aria-expanded") === "true" || false;
+  hamburgerToggle.setAttribute("aria-expanded", !expanded);
   menu.classList.toggle("active");
   hamburgerToggle.classList.toggle("close");
   body.classList.toggle("overflow-hidden");
